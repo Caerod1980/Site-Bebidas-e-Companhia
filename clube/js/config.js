032d80341db2a4e59cd41744580b575c8707f6fd
Caerod1980 <caerod@gmail.com>
2026-06-27 18:43:00 -0300

    Add configuration for API and storage keys

---
+++ clube/js/config.js
@@ -0,0 +1,9 @@
+const API_BASE = 'https://bebcom-delivery-api-1980-bchkauhjf0djajag.brazilsouth-01.azurewebsites.net';
+
+const STORAGE_KEYS = {
+  USER: 'clube_bebcom_user',
+  AVATAR: 'clube_bebcom_avatar'
+};
+
+let currentUser = null;
+let selectedAvatar = null;
